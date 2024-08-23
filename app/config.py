3ee89e2824507f1ddb67c45484860a2495c1c13a
Likith Gowda S <105909257+likithgowda18@users.noreply.github.com>
2024-08-24 00:48:58 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://part1_cb2p_user:y3LtAjJX25q2DdXgP3HEbBUSMUDDYvc3@dpg-cr49u4o8fa8c73djoi5g-a.oregon-postgres.render.com/part1_cb2p")
 
 settings = Settings()
